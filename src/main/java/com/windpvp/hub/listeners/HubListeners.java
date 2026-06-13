e3100bd55644f2af2edf5f737a68f321a1ce3c78
windcolor-dev <79166388+windcolor-dev@users.noreply.github.com>
2026-06-12 18:52:12 -0700

    feat: lobby fly - with hardcoded restricted boundary

--- src/main/java/com/windpvp/hub/listeners/HubListeners.java
+++ src/main/java/com/windpvp/hub/listeners/HubListeners.java
@@ -1,7 +1,9 @@
 package com.windpvp.hub.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 import com.windpvp.hub.HubPatches;
@@ -19,5 +21,25 @@ public class HubListeners implements Listener {
         if (plugin.getHubSpawnLocation() != null) {
             event.setSpawnLocation(plugin.getHubSpawnLocation());
         }
+        Player player = event.getPlayer();
+        if (player.hasPermission("hubpatches.fly")) {
+			player.setAllowFlight(true);
+			player.setFlying(true);
+		} else {
+			player.setAllowFlight(false);
+			player.setFlying(false);
+		}
     }
+    
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+		if (plugin.getHubSpawnLocation() != null) {
+			if (event.getTo().getWorld().equals(plugin.getHubSpawnLocation().getWorld())) {
+				if (event.getTo().distanceSquared(plugin.getHubSpawnLocation()) > (Math.pow(164.274, 2))) {
+					event.getPlayer().teleport(plugin.getHubSpawnLocation());
+					event.getPlayer().sendMessage("§cCome back!!");
+				}
+			}
+		}
+	}
 }
